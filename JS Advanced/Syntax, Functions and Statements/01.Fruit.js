d0cac19a0d3a9212335a7b935c1a99d5903bf61d
Ivan Gechev <ivan_banov@abv.bg>
2023-03-27 16:35:17 +0300

    07.ValidityChecker

--- JS Advanced/Syntax, Functions and Statements/01.Fruit.js
+++ JS Advanced/Syntax, Functions and Statements/01.Fruit.js
@@ -1,8 +1,12 @@
 function moneyNeeded(...input) {
-    let fruit = input[0];
-    let weight = Number(input[1] / 1000);
-    let money = Number(input[2] * weight);
-    console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
+  let fruit = input[0];
+  let weight = Number(input[1] / 1000);
+  let money = Number(input[2] * weight);
+  console.log(
+    `I need $${money.toFixed(2)} to buy ${weight.toFixed(
+      2
+    )} kilograms ${fruit}.`
+  );
 }
 
-moneyNeeded('orange', 2500, 1.80);
+moneyNeeded("orange", 2500, 1.8);
--- JS Advanced/Syntax, Functions and Statements/02.GreatestCommonDivisor.js
+++ JS Advanced/Syntax, Functions and Statements/02.GreatestCommonDivisor.js
@@ -1,11 +1,11 @@
 function solve(a, b) {
-    let result = a % b;
-    while (result != 0) {
-        a = b;
-        b = result;
-        result = a % b;
-    }
-    console.log(b);
+  let result = a % b;
+  while (result != 0) {
+    a = b;
+    b = result;
+    result = a % b;
+  }
+  console.log(b);
 }
 
 solve(15, 5);
