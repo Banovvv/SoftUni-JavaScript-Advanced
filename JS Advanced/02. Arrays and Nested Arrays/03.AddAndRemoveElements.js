e94e48936347bc5d91df7fd97bba3fe3bb4bd873
Ivan Gechev <ivan_banov@abv.bg>
2023-03-28 12:51:46 +0300

    More tasks

---
+++ JS Advanced/02. Arrays and Nested Arrays/03.AddAndRemoveElements.js
@@ -0,0 +1,25 @@
+function addAndRemoveElements(commands) {
+    var result = [];
+
+    for (let i = 1; i <= commands.length; i++) {
+        let command = commands[i - 1];
+
+        if (command == 'add') {
+            result.push(i);
+        } else if (command == 'remove') {
+            result.pop();
+        }
+    }
+
+    if (result.length != 0) {
+        result.forEach(element => {
+            console.log(element);
+        });
+    } else {
+        console.log('Empty');
+    }
+}
+
+addAndRemoveElements(['add', 'add', 'add', 'add']);
+addAndRemoveElements(['add', 'add', 'remove', 'add', 'add']);
+addAndRemoveElements(['remove', 'remove', 'remove'])
---
+++ JS Advanced/02. Arrays and Nested Arrays/04.RotateArray.js
@@ -0,0 +1,11 @@
+function rotateArray(elements, n) {
+    for (let i = 0; i < n; i++) {
+        var element = elements.pop();
+        elements.unshift(element);
+    }
+
+    console.log(elements.join(' '));
+}
+
+rotateArray(['1', '2', '3', '4'], 2);
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple'], 15);
