a349eeede60d828ebb47d713f30e9b1c3a0c3bfc
Ivan Gechev <ivan_banov@abv.bg>
2023-03-28 09:51:40 +0300

    01.PrintAnArrayWithGivenDelimiter

--- JS Advanced/02. Arrays and Nested Arrays/01.PrintAnArrayWithGivenDelimiter.js
+++ JS Advanced/02. Arrays and Nested Arrays/01.PrintAnArrayWithGivenDelimiter.js
@@ -1,3 +1,14 @@
-function printAnArrayWithGivenDelimiter() {
+function printAnArrayWithGivenDelimiter(stringArray, delimiter) {
+    let result = '';
 
+    stringArray.forEach(element => {
+        result = result.concat(delimiter, element);
+    });
+
+    result = result.slice(1);
+
+    console.log(result);
 }
+
+printAnArrayWithGivenDelimiter(['One', 'Two', 'Three', 'Four', 'Five'], '-');
+printAnArrayWithGivenDelimiter(['How about no?', 'I', 'will', 'not', 'do', 'it!'], '_');
