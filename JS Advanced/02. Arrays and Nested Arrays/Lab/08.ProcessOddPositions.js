51e4a0defcc6053a5ffaec1d36c0c4cb75929361
Ivan Gechev <ivan_banov@abv.bg>
2023-03-29 12:35:32 +0300

    08.ProcessOddPositions

---
+++ JS Advanced/02. Arrays and Nested Arrays/Lab/08.ProcessOddPositions.js
@@ -0,0 +1,12 @@
+function processOddPositions(numbers) {
+    let result = [];
+
+    for (let i = 1; i < numbers.length; i+=2) {
+        result.unshift(numbers[i] * 2);
+    }
+
+    console.log(result.join(' '));
+}
+
+processOddPositions([10, 15, 20, 25]);
+processOddPositions([3, 0, 10, 4, 7, 3]);
