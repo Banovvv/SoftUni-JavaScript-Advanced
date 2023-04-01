fb75bb991d082bce8803ad28c642915fc53425d3
Ivan Gechev <ivan_banov@abv.bg>
2023-04-01 20:32:16 +0300

    01.CalorieObject

---
+++ JS Advanced/03. Objects and Composition/01.CalorieObject.js
@@ -0,0 +1,13 @@
+function calorieObject(params) {
+    let foods = {};
+
+    for (let i = 0; i < params.length; i++) {
+        if (i % 2 == 0) {
+            foods[`${params[i]}`] = params[i + 1];
+        }
+    }
+
+    console.log(foods);
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
--- JS Advanced/03. Objects and Composition/Lab/01.CityRecord.js
+++ JS Advanced/03. Objects and Composition/Lab/01.CityRecord.js
@@ -1,4 +1,4 @@
-function cityRecord(name, population, treasury){
+function cityRecord(name, population, treasury) {
     return {
         name: name,
         population: population,
@@ -6,5 +6,5 @@ function cityRecord(name, population, treasury){
     }
 }
 
-console.log(cityRecord('Tortuga', 7000, 15000 ));
-console.log(cityRecord('Santo Domingo', 12000, 23500 ));
+console.log(cityRecord('Tortuga', 7000, 15000));
+console.log(cityRecord('Santo Domingo', 12000, 23500));
