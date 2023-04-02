17c901ce5cfc1f38eb8a62c97dff9519b2d09af1
Ivan Gechev <ivan_banov@abv.bg>
2023-04-02 11:00:42 +0300

    Resolve-CookingByNumbers.

---
+++ JS Advanced/03. Objects and Composition/Lab/Resolve-CookingByNumbers.js
@@ -0,0 +1,20 @@
+function cookingByNumbers(number, ...params) {
+    console.log(number);
+
+    const actions = {
+        chop() { number /= 2; },
+        dice() { number = Math.sqrt(number); },
+        spice() { number += 1; },
+        bake() { number *= 3; },
+        fillet() { number = (number * 0.80).toFixed(1); }
+    };
+
+    params.forEach(comand => {
+        actions[comand]();
+
+        console.log(number);
+    });
+}
+
+cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
