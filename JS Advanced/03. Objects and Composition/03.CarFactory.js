0939c2f8ea7af184e7cd14310a5c00cc027cc2e6
Ivan Gechev <ivan_banov@abv.bg>
2023-04-03 18:43:13 +0300

    03.CarFactory

---
+++ JS Advanced/03. Objects and Composition/03.CarFactory.js
@@ -0,0 +1,68 @@
+const engines = {
+    smallEngine: function (car) {
+        car.engine = {
+            power: 90,
+            volume: 1800
+        }
+    },
+    normalEngine: function (car) {
+        car.engine = {
+            power: 120,
+            volume: 2400
+        }
+    },
+    monsterEngine: function (car) {
+        car.engine = {
+            power: 200,
+            volume: 3500
+        }
+    }
+}
+
+const carriages = {
+    hatchback: function (car, object) {
+        car.carriage = {
+            type: 'hatchback',
+            color: object.color
+        }
+    },
+    coupe: function (car, object) {
+        car.carriage = {
+            type: 'coupe',
+            color: object.color
+        }
+    }
+}
+
+function carFactory(object) {
+    let car = {
+        model: object.model
+    };
+
+    if (object.power <= 90) {
+        engines.smallEngine(car);
+    } else if (object.power <= 123) {
+        engines.normalEngine(car);
+    } else {
+        engines.monsterEngine(car);
+    }
+
+    if (object.carriage == 'hatchback') {
+        carriages.hatchback(car, object);
+    } else if (object.carriage == 'coupe') {
+        carriages.coupe(car, object);
+    }
+
+    car.wheels = new Array(4)
+        .fill(object.wheelsize);
+
+    return car;
+}
+
+let car = carFactory({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+});
