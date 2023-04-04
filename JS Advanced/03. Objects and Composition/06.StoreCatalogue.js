942c23dc396138fe0c1569437db268a85746f609
Ivan Gechev <ivan_banov@abv.bg>
2023-04-04 18:32:29 +0300

    06.StoreCatalogue

---
+++ JS Advanced/03. Objects and Composition/06.StoreCatalogue.js
@@ -0,0 +1,36 @@
+function storeCatalogue(input) {
+    input.sort((a, b) => a.localeCompare(b));
+    let char = '';
+
+    input.forEach(item => {
+        let [product, price] = item.split(' : ');
+
+        if(product[0]!= char){
+            console.log(product[0]);
+        }
+
+        console.log(`  ${product}: ${price}`);
+
+        char = product[0];
+    });
+}
+
+storeCatalogue([
+    'Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']);
+
+storeCatalogue([
+    'Banana : 2', 
+    'Rubics Cube : 5', 
+    'Raspberry P : 4999', 
+    'Rolex : 100000', 
+    'Rollon : 10', 
+    'Rali Car : 2000000', 
+    'Pesho : 0.000001', 
+    'Barrel : 10']);
