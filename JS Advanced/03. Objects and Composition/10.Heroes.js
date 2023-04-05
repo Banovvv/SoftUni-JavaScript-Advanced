2264b9982b33a156f12a3608f9b83899681eff2e
Ivan Gechev <ivan_banov@abv.bg>
2023-04-05 10:15:46 +0300

    10.Heroes

---
+++ JS Advanced/03. Objects and Composition/10.Heroes.js
@@ -0,0 +1,36 @@
+function heroFactory() {
+    return {
+        mage(name) {
+            return {
+                name,
+                mana: 100,
+                cast(spell) {
+                    this.mana--;
+                    console.log(`${this.name} cast ${spell}`);
+                }
+            };
+        },
+        fighter(name) {
+            return {
+                name,
+                stamina: 100,
+                fight() {
+                    this.stamina--;
+                    console.log(`${this.name} slashes at the foe!`);
+                }
+            };
+        }
+    }
+}
+
+let create = heroFactory();
+
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight();
+console.log(scorcher2.stamina);
+console.log(scorcher.mana); 
