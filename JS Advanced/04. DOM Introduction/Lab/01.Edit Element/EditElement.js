0183d98252a855883a9322c09c3405f34eaee289
Ivan Gechev <ivan_banov@abv.bg>
2023-04-06 13:55:45 +0300

    01.Edit Element

--- JS Advanced/04. DOM Introduction/Lab/01.Edit Element/EditElement.js
+++ JS Advanced/04. DOM Introduction/Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,4 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    let regex = new RegExp(match, 'g');
+    element.textContent = element.textContent.replace(regex, replacer);
 }
