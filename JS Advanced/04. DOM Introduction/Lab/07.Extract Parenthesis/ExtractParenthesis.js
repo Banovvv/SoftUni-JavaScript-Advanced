c360b954a58dbbe1e18fd88ea87ba6c6d1407248
Ivan Gechev <ivan_banov@abv.bg>
2023-04-07 11:07:14 +0300

    07.Extract Parenthesis

--- JS Advanced/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+    let contentParagraph = document.getElementById('content');
+    let matches = contentParagraph.textContent
+        .matchAll(/\((?<name>[^\)]+)\)/g);
 
+    let result = [];
+
+    for (let match of matches) {
+        result.push(match.groups.name);
+    }
+
+    contentParagraph.textContent = result.join('; ');
 }
