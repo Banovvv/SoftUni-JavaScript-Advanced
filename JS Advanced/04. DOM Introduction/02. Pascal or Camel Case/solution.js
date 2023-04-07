a02f47fd9e43b0a1affd6c29789aa57b33c13423
Ivan Gechev <ivan_banov@abv.bg>
2023-04-07 12:59:24 +0300

    02. Pascal or Camel Case

--- JS Advanced/04. DOM Introduction/02. Pascal or Camel Case/solution.js
+++ JS Advanced/04. DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+  let resultContainer = document.getElementById('result');
+
+  resultContainer.textContent = inputToCase(input, namingConvention);
+}
+
+function inputToCase(input, namingConvention) {
+  let result = [];
+  let elements = input.split(' ');
+
+  if (namingConvention === 'Camel Case') {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i] = elements[i].toLowerCase();
+
+      if (i === 0) {
+        result.push(elements[i]);
+      } else {
+        elements[i] = elements[i].charAt(0).toUpperCase() + elements[i].slice(1);
+        result.push(elements[i]);
+      }
+    }
+  } else if (namingConvention === 'Pascal Case') {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i] = elements[i].toLowerCase();
+      elements[i] = elements[i].charAt(0).toUpperCase() + elements[i].slice(1);
+
+      result.push(elements[i]);
+    }
+  } else {
+    result.push('Error!');
+  }
+
+  return result.join('');
 }
