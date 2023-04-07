03bcad0b6922e4d9fc47f09a437feb141058d5e2
Ivan Gechev <ivan_banov@abv.bg>
2023-04-07 10:48:52 +0300

    06.Sum Table

--- JS Advanced/04. DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ JS Advanced/04. DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,17 @@
 function sumTable() {
+    let sumCell = document.getElementById('sum');
 
+    if (sumCell.textContent === '') {
+        let prices = document.querySelectorAll('td');
+        let sum = 0;
+
+        for (let i = 1; i < prices.length; i += 2) {
+            const element = prices[i];
+            sum += Number(element.textContent);
+        }
+
+        sumCell.textContent = sum;
+    } else { // Extra stuff
+        alert('The sum is already calculated');
+    }
 }
