3cf74ab66e295a9c2e95d7ad1e3f026223abe13c
Ivan Gechev <ivan_banov@abv.bg>
2023-04-08 08:47:07 +0300

    03. Accordion

--- JS Advanced/04. DOM Introduction/03. Accordion/accordion.js
+++ JS Advanced/04. DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.getElementsByClassName('button');
+    let text = document.getElementById('extra');
+
+    if (button[0].textContent === 'More') {
+        text.style.display = 'block';
+        button[0].textContent = 'Less';
+    } else {
+        text.style.display = 'none';
+        button[0].textContent = 'More';
+    }
 }
