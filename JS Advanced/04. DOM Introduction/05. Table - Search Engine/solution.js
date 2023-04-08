00928fafc2165bb8df2e17f71203b0fd50a91bae
Ivan Gechev <ivan_banov@abv.bg>
2023-04-08 20:05:55 +0300

    05. Table - Search Engine

--- JS Advanced/04. DOM Introduction/05. Table - Search Engine/solution.js
+++ JS Advanced/04. DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let searchFor = document.getElementById('searchField').value;
+      let elements = document.querySelectorAll('tbody td');
 
+      elements.forEach(element => {
+         element.parentElement.classList.remove('select');
+      });
+
+      elements.forEach(element => {
+         if (element.textContent.toLowerCase().includes(searchFor.toLowerCase())) {
+            element.parentElement.classList.add('select');
+         }
+      });
    }
 }
