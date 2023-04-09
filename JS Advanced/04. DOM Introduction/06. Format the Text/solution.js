14ef2d47a9c75e3a6220b91c03a31f9e2e539881
Ivan Gechev <ivan_banov@abv.bg>
2023-04-09 10:00:48 +0300

    06. Format the Text

--- JS Advanced/04. DOM Introduction/06. Format the Text/solution.js
+++ JS Advanced/04. DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+  let inputText = document.getElementById('input').value;
+  let output = document.getElementById('output');
+
+  let paragraphs = inputText
+    .split('.')
+    .filter(x => x !== '');
+
+  while (paragraphs.length > 0) {
+    let paragraph = paragraphs.splice(0, 3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = paragraph;
+    output.appendChild(p);
+  }
 }
