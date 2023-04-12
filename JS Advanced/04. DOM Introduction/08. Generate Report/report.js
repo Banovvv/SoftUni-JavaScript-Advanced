b8dbb8a89af1cdb4476507766063961d4ee0a3c6
Ivan Gechev <ivan_banov@abv.bg>
2023-04-12 07:57:32 +0300

    08. Generate Report

--- JS Advanced/04. DOM Introduction/08. Generate Report/report.js
+++ JS Advanced/04. DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,29 @@
 function generateReport() {
-    //TODO
+    let columns = document.querySelectorAll('input');
+    let rows = document.querySelectorAll('tbody tr');
+
+    let employees = [];
+    let checkedColumns = [];
+
+    for (let i = 0; i < columns.length; i++) {
+        if (columns[i].checked) {
+            checkedColumns.push(i);
+        }
+    }
+
+    for (let i = 0; i < rows.length; i++) {
+        let employee = {};
+        let employeeParts = rows[i].children;
+
+        checkedColumns.forEach(index => {
+            let key = columns[index].name.toLowerCase();
+            let value = employeeParts[index].textContent;
+
+            employee[key] = value;
+        });
+
+        employees.push(employee);
+    }
+
+    document.getElementById('output').textContent = JSON.stringify(employees);
 }
