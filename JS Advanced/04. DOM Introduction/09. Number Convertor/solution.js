dd93ff6ab8c13a7fd904b7052601c0511a22d628
Ivan Gechev <ivan_banov@abv.bg>
2023-04-12 08:04:46 +0300

    09. Number Convertor

--- JS Advanced/04. DOM Introduction/09. Number Convertor/solution.js
+++ JS Advanced/04. DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    const selectMenu = document.getElementById('selectMenuTo');
 
-    //TODO...
+    const binary = document.createElement('option');
+    binary.textContent = 'Binary';
+    binary.value = 'binary';
+
+    const hexadecimal = document.createElement('option');
+    hexadecimal.textContent = 'Hexadecimal';
+    hexadecimal.value = 'hexadecimal';
+
+    selectMenu.appendChild(binary);
+    selectMenu.appendChild(hexadecimal);
+
+    const selectMap = {
+        'binary': num => num.toString(2),
+        'hexadecimal': num => num.toString(16).toUpperCase()
+    }
+
+    const button = document.querySelector('#container > button');
+
+    button.addEventListener('click', (evt) => {
+        const input = document.getElementById('input');
+        const output = document.getElementById('result');
+
+        output.value = selectMap[selectMenu.value](+input.value);
+    })
 }
