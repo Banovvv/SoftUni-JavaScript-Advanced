0c249201fbc49e3d8fd713a994f901a4bb4212a3
Ivan Gechev <ivan_banov@abv.bg>
2023-04-20 08:03:33 +0300

    02. Add-Delete

--- JS Advanced/05. DOM Manipulations and Events/Lab/02. Add-Delete/app.js
+++ JS Advanced/05. DOM Manipulations and Events/Lab/02. Add-Delete/app.js
@@ -1,3 +1,30 @@
 function addItem() {
-    //TODO...
+    let itemsList = document.getElementById('items');
+    let newItemText = document.getElementById('newItemText');
+
+    let listItemElement = document.createElement('li');
+    listItemElement.textContent = newItemText.value;
+
+    addDeleteLink(listItemElement);
+
+    itemsList.appendChild(listItemElement);
+
+    newItemText.value = '';
+}
+
+function addDeleteLink(element) {
+    let deleteElement = document.createElement('a');
+    deleteElement.setAttribute('href', '#');
+    deleteElement.textContent = '[Delete]';
+    deleteElement.addEventListener('click', event => {
+        const target = event.currentTarget.parentNode;
+        target.parentNode.removeChild(target)
+    });
+
+    element.appendChild(deleteElement);
+}
+
+function deleteElement(event) {
+    const target = event.currentTarget;
+    target.parentNode.removeChild(target);
 }
