bcb8bab40b3cedd62fabfb7a2912e577eb490463
Ivan Gechev <ivan_banov@abv.bg>
2023-04-22 08:40:39 +0300

    03. Delete-From-Table

--- JS Advanced/05. DOM Manipulations and Events/Lab/03. Delete-From-Table/app.js
+++ JS Advanced/05. DOM Manipulations and Events/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let result = 'Not found.';
+    let input = document.querySelector('input[name="email"]');
+
+    let cells = document.querySelectorAll('td');
+
+    cells.forEach(cell => {
+        if (cell.textContent === input.value) {
+            let parent = cell.parentElement;
+            parent.parentElement.removeChild(parent);
+            result = 'Deleted';
+        }
+    });
+
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = result;
+    input.value = '';
 }
