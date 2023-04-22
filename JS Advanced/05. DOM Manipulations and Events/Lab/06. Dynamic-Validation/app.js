16f1a005e2e4114a897e1aca2a42ed72aced61ba
Ivan Gechev <ivan_banov@abv.bg>
2023-04-22 17:59:02 +0300

    06. Dynamic-Validation

--- JS Advanced/05. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
+++ JS Advanced/05. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let email = document.getElementById('email');
+    const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    email.addEventListener('change', event => {
+        if (event.target.value.match(regex)) {
+            event.target.removeAttribute('class');
+            return;
+        }
+
+        email.className = 'error';
+    });
 }
