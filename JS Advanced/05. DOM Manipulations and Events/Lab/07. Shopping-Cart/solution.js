cca0797409cda00c2ce5982841c56a54259339c4
Ivan Gechev <ivan_banov@abv.bg>
2023-04-23 10:58:23 +0300

    07. Shopping-Cart

--- JS Advanced/05. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
+++ JS Advanced/05. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,35 @@
 function solve() {
-   //TODO...
+   let textArea = document.querySelector('textarea');
+
+   const buttons = document.querySelectorAll('.add-product');
+   buttons.forEach(x => x.addEventListener('click', clickEvent));
+
+   const btnCheckOut = document.querySelector('button.checkout');
+   btnCheckOut.addEventListener('click', clickCheckOutEvent)
+
+   let sum = 0;
+   const list = [];
+
+   function clickEvent(event) {
+      const name = event.target.parentElement.parentElement
+         .querySelector('.product-title').textContent;
+      const price = Number(event.target.parentElement.parentElement
+         .querySelector('.product-line-price').textContent);
+
+      sum += price;
+
+      if (!list.includes(name)) {
+         list.push(name);
+      }
+
+      const output = `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+      textArea.value += output;
+   }
+
+
+   function clickCheckOutEvent() {
+      textArea.value += `You bought ${list.join(', ')} for ${sum.toFixed(2)}.`
+      buttons.forEach(x => x.removeEventListener('click', clickEvent));
+      btnCheckOut.removeEventListener('click', clickCheckOutEvent)
+   }
 }
