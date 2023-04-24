d622ab190a2cecfce27978c816eedbf59e84e521
Ivan Gechev <ivan_banov@abv.bg>
2023-04-24 20:51:26 +0300

    01. Sections

--- JS Advanced/05. DOM Manipulations and Events/01. Sections/script.js
+++ JS Advanced/05. DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentElement = document.getElementById('content');
+
+   words.forEach(word => {
+      let wordElement = document.createElement('div');
+      let paragraphElement = document.createElement('p');
+      paragraphElement.textContent = word;
+      paragraphElement.style.display = "none";
+
+      wordElement.appendChild(paragraphElement);
+      wordElement.addEventListener('click', event => {
+         paragraphElement.style.display = "block";
+      });
+
+      contentElement.appendChild(wordElement);
+   });
 }
