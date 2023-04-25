4c4ad3f89279cab200b414e0095695377e05ac89
Ivan Gechev <ivan_banov@abv.bg>
2023-04-25 21:15:55 +0300

    03. Locked-Profile

--- JS Advanced/05. DOM Manipulations and Events/03. Locked-Profile/app.js
+++ JS Advanced/05. DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('button');
+
+    for (const button of buttons) {
+        button.addEventListener('click', clickEvent);
+    }
+
+    function clickEvent(event) {
+        let profile = event.target.parentElement;
+        let lock = event.target.parentElement
+            .querySelector('input[value="lock"]');
+
+        if (lock.checked != true) {
+            let hiddenInfo = Array.from(profile.querySelectorAll('div'))
+                .find(x => x.id.includes('HiddenFields'));
+
+            if (event.target.textContent === 'Show more') {
+                event.target.textContent = 'Hide it'
+                hiddenInfo.style.display = 'block'
+            } else {
+                event.target.textContent = 'Show more'
+                hiddenInfo.style.display = 'none'
+            }
+        }
+    }
 }
