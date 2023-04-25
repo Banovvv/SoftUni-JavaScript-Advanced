96a2ff6322fe57e51b47869102a53411600a7335
Ivan Gechev <ivan_banov@abv.bg>
2023-04-25 21:26:43 +0300

    04. Fill-Dropdown

--- JS Advanced/05. DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/05. DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let menuElement = document.getElementById('menu');
+    let textElement = document.getElementById('newItemText');
+    let valueElement = document.getElementById('newItemValue');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = textElement.value;
+    optionElement.value = valueElement.value
+
+    menuElement.appendChild(optionElement);
+
+    textElement.value = '';
+    valueElement.value = '';
 }
