81a3a4b034245a12613129db6041ac0de3749330
Ivan Gechev <ivan_banov@abv.bg>
2023-04-26 09:03:04 +0300

    05. Encode-and-Decode-Messages

--- JS Advanced/05. DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/05. DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,40 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('button');
+    let inputArea = document.getElementsByTagName('textarea')[0];
+    let outputArea = document.getElementsByTagName('textarea')[1];
+
+    buttons.forEach(button => {
+        if (button.textContent === 'Encode and send it') {
+            button.addEventListener('click', encodeEvent);
+        } else if (button.textContent === 'Decode and read it') {
+            button.addEventListener('click', decodeEvent);
+        }
+    });
+
+    function encodeEvent(event) {
+        let encodedMessage = '';
+
+        inputArea.value
+            .split('')
+            .forEach(letter => {
+                encodedMessage += String
+                    .fromCharCode(letter.charCodeAt(0) + 1);
+            });
+
+        outputArea.value = encodedMessage;
+        inputArea.value = '';
+    }
+
+    function decodeEvent(event) {
+        let decodedMessage = '';
+
+        outputArea.value
+            .split('')
+            .forEach(letter => {
+                decodedMessage += String
+                    .fromCharCode(letter.charCodeAt(0) - 1);
+            });
+
+        outputArea.value = decodedMessage;
+    }
 }
